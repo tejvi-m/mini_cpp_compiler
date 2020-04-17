5ca3343a851a747ba254c996675d7c1bc649524a
tejvi-m <tejvimstays@gmail.com>
2020-04-17 22:24:41 +0530

    variable propogation and constant folding ?

--- codeOpt/opt.cpp
+++ codeOpt/opt.cpp
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <ctype.h>
+#include <boost/lexical_cast.hpp>
 // #include "tinyexpr.h"
 
 
@@ -16,6 +17,25 @@ using namespace std;
 unordered_map<string, float> IDs;
 vector<string> res;
 
+float eval(vector<string>& words){
+
+  if(words[3] == "+"){
+    return boost::lexical_cast<float>(words[2]) + boost::lexical_cast<float>(words[4]);
+  }
+  if(words[3] == "-"){
+return boost::lexical_cast<float>(words[2]) - boost::lexical_cast<float>(words[4]);
+  }
+  if(words[3] == "*"){
+    return boost::lexical_cast<float>(words[2]) * boost::lexical_cast<float>(words[4]);
+  }
+  if(words[3] == "/"){
+    return boost::lexical_cast<float>(words[2]) / boost::lexical_cast<float>(words[4]);
+  }
+  else if(words[3] == "%"){
+      return boost::lexical_cast<int>(words[2]) % boost::lexical_cast<int>(words[4]);
+  }
+}
+
 bool is_number(const std::string& s)
 {
     std::string::const_iterator it = s.begin();
@@ -25,6 +45,7 @@ bool is_number(const std::string& s)
 
 void variable_propogate(vector<string>& program, unordered_set<string>& ignore){
   vector<string> words;
+
   bool flag = 0;
   string expr = "";
   for(auto line: program){
@@ -37,6 +58,7 @@ void variable_propogate(vector<string>& program, unordered_set<string>& ignore){
           flag = 0;
           expr = "";
           boost::split(words, line, boost::is_any_of(" "), boost::token_compress_on);
+          words.erase(remove(words.begin(), words.end(), ""), words.end());
           if(ignore.find(words[0]) == ignore.end()){
             cout << words[1] << endl;
             if(words.size() > 1 && words[1] == "="){
@@ -44,15 +66,23 @@ void variable_propogate(vector<string>& program, unordered_set<string>& ignore){
               for(int i = 2; i < words.size(); i++){
                 if(!is_number(words[i])){
                   if(IDs.find(words[i]) != IDs.end()){
-                    words[i] = IDs[words[i]];
+                    words[i] =  to_string(IDs[words[i]]);
                     flag = 1;
+                  }
+                }
+                else{
+                  if(words.size() == 3){
+                     IDs[words[0]] = boost::lexical_cast<float>(words[2]);
                   }
                 }
                 expr += words[i];
               }
             }
-            IDs[words[0]] = 2;
-            res.push_back(words[0] + " = " + to_string(IDs[words[0]]));
+            if(words.size() == 5){
+              IDs[words[0]] = eval(words);
+              res.push_back(words[0] + " = " + to_string(IDs[words[0]]));
+            }
+            else res.push_back(line);
 
         }
         }
