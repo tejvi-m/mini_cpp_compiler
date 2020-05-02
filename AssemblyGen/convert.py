e595f24f97f5e6fa7a7e9fb81c48ddc5cac6c34c
tejvi-m <tejvimstays@gmail.com>
2020-05-02 20:49:48 +0530

    labels: printing as is

--- AssemblyGen/convert.py
+++ AssemblyGen/convert.py
@@ -7,8 +7,13 @@ if __name__ == "__main__":
     file = open("input.txt").readlines()
 
     for line in file:
+        line = line.strip()
         if "=" in line:
             # assignment
             print(generator.convert_eq(line))
+        elif line[-1] == ":" and len(line.split()) == 1:
+            # label
+            print(line)
+    
         else:
             pass
