1e5e4f9a9793b11ceaae4dc8a30a59b28064ce13
tejvi-m <tejvimstays@gmail.com>
2020-05-03 12:13:26 +0530

    generating addresses for instructions

--- AssemblyGen/cvt.py
+++ AssemblyGen/cvt.py
@@ -1,6 +1,8 @@
 class convert():
     registers = {"%rax", "%rbx", "%rcx", "%rdx", "%rdi", "%rsi", "%rbp", "%rsp", "%r8", "%r9", "%r10", "%r11", "%r12", "%r13", "%r14", "%r15"}
     mapping = dict()
+    labelMapping = dict()
+    pc = 0
 
     arithmeticOps = {"+", "-", "/", "*"}
 
@@ -13,6 +15,12 @@ class convert():
             return 1
         except:
             return 0
+
+    def genAdd(self):
+        x = self.pc
+        self.pc += 1
+        return "<" + "{:010d}".format(x) + ">   "
+        
 
     def pullFromMap(self):
         pass
@@ -62,79 +70,79 @@ class convert():
             
             src = self.getSrc(tokens[2])
 
-            return "mov " + src + ", " + dest_reg
+            return self.genAdd() + "mov " + src + ", " + dest_reg
             
             
         else:
             if len(expr) == 2 and expr[0] == "not":
-                return "compl $0x0, " + self.getSrc(expr[1]) + "\n" + \
-                        "setle %al"+ "\n" + \
-                        "movzbl %al, %eax\n" + \
-                        "mov %eax, " + dest_reg
+                return self.genAdd() + "compl $0x0, " + self.getSrc(expr[1]) + "\n" + \
+                        self.genAdd() + "setle %al"+ "\n" + \
+                        self.genAdd() + "movzbl %al, %eax\n" + \
+                        self.genAdd() + "mov %eax, " + dest_reg
 
             src1 = self.getSrc(expr[0])
             src2 = self.getSrc(expr[2])
 
             # based on disas on gdb
             if "<" == expr[1]:
-                return "compl " + src1 + ", " + src2 + "\n" + \
-                        "setle %al"+ "\n" + \
-                        "movzbl %al, %eax\n" + \
-                        "mov %eax, " + dest_reg
+                return self.genAdd() +"compl " + src1 + ", " + src2 + "\n" + \
+                        self.genAdd() +"setle %al"+ "\n" + \
+                        self.genAdd() +"movzbl %al, %eax\n" + \
+                        self.genAdd() +"mov %eax, " + dest_reg
             
             elif ">" == expr[1]:
-                return "compl " + src1 + ", " + src2 + "\n" + \
-                            "setge %al"+ "\n" + \
-                            "movzbl %al, %eax\n" + \
-                            "mov %eax, " + dest_reg
+                return self.genAdd() +"compl " + src1 + ", " + src2 + "\n" + \
+                        self.genAdd() +    "setge %al"+ "\n" + \
+                        self.genAdd() +    "movzbl %al, %eax\n" + \
+                        self.genAdd() +    "mov %eax, " + dest_reg
                 
 
             elif ">=" == expr[1]:
-                return "compl " + src1 + ", " + src2 + "\n" + \
-                            "setg %al"+ "\n" + \
-                            "movzbl %al, %eax\n" + \
-                            "mov %eax, " + dest_reg
+                return self.genAdd() +"compl " + src1 + ", " + src2 + "\n" + \
+                          self.genAdd() +  "setg %al"+ "\n" + \
+                          self.genAdd() +  "movzbl %al, %eax\n" + \
+                           self.genAdd() + "mov %eax, " + dest_reg
 
             elif "<=" == expr[1]:
-                return "compl " + src1 + ", " + src2 + "\n" + \
-                            "setl %al"+ "\n" + \
-                            "movzbl %al, %eax\n" + \
-                            "mov %eax, " + dest_reg
+                return self.genAdd() +"compl " + src1 + ", " + src2 + "\n" + \
+                        self.genAdd() +    "setl %al"+ "\n" + \
+                         self.genAdd() +   "movzbl %al, %eax\n" + \
+                          self.genAdd() +  "mov %eax, " + dest_reg
             
             elif "==" == expr[1]:
-                return "compl " + src1 + ", " + src2 + "\n" + \
-                            "sete %al"+ "\n" + \
-                            "movzbl %al, %eax\n" + \
-                            "mov %eax, " + dest_reg
+                return self.genAdd() +"compl " + src1 + ", " + src2 + "\n" + \
+                        self.genAdd() +    "sete %al"+ "\n" + \
+                        self.genAdd() +    "movzbl %al, %eax\n" + \
+                        self.genAdd() +    "mov %eax, " + dest_reg
 
             elif "!=" == expr[1]:
-                return "compl " + src1 + ", " + src2 + "\n" + \
-                            "sete %al"+ "\n" + \
-                            "movzbl %al, %eax\n" + \
-                            "mov %eax, " + dest_reg
+                return self.genAdd() +"compl " + src1 + ", " + src2 + "\n" + \
+                        self.genAdd() +    "sete %al"+ "\n" + \
+                        self.genAdd() +    "movzbl %al, %eax\n" + \
+                         self.genAdd() +   "mov %eax, " + dest_reg
 
             elif "+" == expr[1]:
-                return "mov " + src1 + ", %edx\n" + \
-                        "mov " + src2 + ", %eax\n" + \
-                        "add %eax, %edx\n" + \
-                        "mov %eax, " + dest_reg
+                return self.genAdd() +"mov " + src1 + ", %edx\n" + \
+                       self.genAdd() + "mov " + src2 + ", %eax\n" + \
+                        self.genAdd() +"add %eax, %edx\n" + \
+                       self.genAdd() + "mov %eax, " + dest_reg
             elif "-" == expr[1]:
-                return "mov " + src1 + ", %edx\n" + \
-                        "mov " + src2 + ", %eax\n" + \
-                        "add %eax, %edx\n" + \
-                        "mov %eax, " + dest_reg
+                return self.genAdd() +"mov " + src1 + ", %edx\n" + \
+                       self.genAdd() + "mov " + src2 + ", %eax\n" + \
+                       self.genAdd() + "add %eax, %edx\n" + \
+                       self.genAdd() + "mov %eax, " + dest_reg
 
             elif "*" == expr[1]:
-                    return "mov " + src1 + ", %edx\n" + \
-                            "mov " + src2 + ", %eax\n" + \
-                            "imul /*THIS IS WRONG*/ %eax, %edx\n" + \
-                            "mov %eax, " + dest_reg
+                    return self.genAdd() +"mov " + src1 + ", %edx\n" + \
+                            self.genAdd() +"mov " + src2 + ", %eax\n" + \
+                           self.genAdd() + "imul /*THIS IS WRONG*/ %eax, %edx\n" + \
+                           self.genAdd() + "mov %eax, " + dest_reg
 
             elif "/" == expr[1]:
-                return "mov " + src1 + ", %edx\n" + \
-                        "idivl /*THIS IS WRONG*/ " + src2 + ", %eax\n" + \
-                        "add %eax, %edx\n" + \
-                        "mov %eax, " + dest_reg
+                return self.genAdd() +"mov " + src1 + ", %edx\n" + \
+                        self.genAdd() +"idivl /*THIS IS WRONG*/ " + src2 + ", %eax\n" + \
+                        self.genAdd() +"add %eax, %edx\n" + \
+                       self.genAdd() + "mov %eax, " + dest_reg
 
 
 
@@ -143,10 +151,10 @@ class convert():
 
     def convert_if_branch(self, line):
         tokens = line.split()
-        return "cmpl $0x0, " + self.getSrc(tokens[1]) + "\n" + \
-                "jne " + self.getAddr(tokens[3])
+        return self.genAdd() +"cmpl $0x0, " + self.getSrc(tokens[1]) + "\n" + \
+                self.genAdd() +"jne " + self.getAddr(tokens[3])
 
  
     def cvt_goto(self, line):
         tokens = line.strip().split()
-        return "jmp " + self.getAddr(tokens[1])
+        return self.genAdd() +"jmp " + self.getAddr(tokens[1])
